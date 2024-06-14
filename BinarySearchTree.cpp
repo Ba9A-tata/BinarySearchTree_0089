db4eb88ff3d504c2980fabb05cf7b450c22afc69
Ba9A-tata <yogadzakkii@gmail.com>
2024-06-14 14:06:55 +0700

    menambahkan void search ini digunakan untuk mencari

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -20,5 +20,44 @@ public:
 
 class BinaryTree
 {
+public:
+    Node* ROOT;
     
+    BinaryTree()
+    {
+        ROOT = NULL; // Initializing  ROOT to null
+    }
+
+    void insert(string element) // insert a node in the binary search tree
+    {
+        Node* newNode = new Node(element, NULL, NULL); // Allocate memory for the new node
+        newNode-> info = element; // Assign value to the data field fo the new node
+        newNode-> leftchild = NULL; // Make the left child of the new node point to NULL
+        newNode-> rightchild = NULL; // Make the right child of the new node point to NULL
+
+        Node* parrent = NULL;
+        Node* currentNode = NULL;
+        search(element, parrent, currentNode); // Locate the node wich will be the  parrent of the node to be inserted 
+
+        if (parrent == NULL) // if the parrent is NULL (Tree is empty)
+        {
+            ROOT = newNode; // Mark the new node as ROOT
+            return; // Exit
+        }
+        if (element < parrent ->info) // if hte value in the data field of the new node is less than that of the parrent
+        {
+            parrent->leftchild = newNode; // Make the left child of the parrent point to the new node
+        }
+        else if (element > parrent->info) // if the value in the data field of the new node is greater than that of the parrent
+        {
+            parrent->rightchild = newNode; // Make the right child of the parrent point to the new node
+        }    
+    }
+
+    void search(string element, Node*& parrent, Node*& currentNode)
+    {
+        
+    }
+
+
 }
